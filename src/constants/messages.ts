f7e19b1b3afb3137bf405a72ca126c80737d2470
xinchaoduyanh <duyanh19122k3@gmail.com>
2024-09-10 20:10:37 +0700

    Update Schema

---
+++ src/constants/messages.ts
@@ -0,0 +1,10 @@
+export const USERS_MESSAGES = {
+  VALIDATION_ERROR: 'Validation error',
+  EMAIL_ALREADY_EXISTS: 'Email already exists',
+  EMAIL_NOT_FOUND: 'Email not found',
+  INVALID_EMAIL_OR_PASSWORD: 'Invalid email or password',
+  UNAUTHORIZED: 'Unauthorized',
+  FORBIDDEN: 'Forbidden',
+  NOT_FOUND: 'Not found',
+  INTERNAL_SERVER_ERROR: 'Internal server error'
+}
